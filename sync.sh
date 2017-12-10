7b2cf6449a958073fef09d11c91c84976c9f15b9
Andrej Čremožnik <andrej@cremoznik.si>
2017-12-11 00:47:53 +0100

    Configure sync

--- sync.sh
+++ sync.sh
@@ -10,14 +10,12 @@
 # 6. perform a search-replace for site URL and flush rewrite rules
 # 7. create an admin user for development (dev / dev)
 #
-# Review TODOs below to setup the script
-#
 
-# TODO: Set server SSH connection parameters
-ssh_connection="user@host -p 54321"
+# Set server SSH connection parameters
+ssh_connection="skye@cremoznik.si -p 52022"
 
-# TODO: Set path to the WordPress installation directory on the server
-remote_wordpress="/srv/http/crean.dev/releases/current/web/wp"
+# Set path to the WordPress installation directory on the server
+remote_wordpress="/srv/http/cremoznik.si/current/web/wp"
 
 function findWPCLI {
   command -v wp > /dev/null 2>&1 || { echo >&2 "==> WP-CLI needs to be available as 'wp' command in your PATH $1"; exit 1; }
@@ -31,9 +29,9 @@ wp db reset --yes
 echo -e "==> Importing database from production…"
 ssh -C $ssh_connection "wp --path=$remote_wordpress db export -" | wp db import -
 
-# TODO: Deactivate plugins you don't want locally
-#echo -e "==> Deactivating production only plugins…"
-#wp plugin deactivate relevanssi wordpress-seo
+# Deactivate plugins you don't want locally
+echo -e "==> Deactivating production only plugins…"
+wp plugin deactivate redis-cache wordpress-seo
 
 echo -e "==> Cleaning up…"
 wp comment delete $(wp comment list --status=spam --format=ids) --force
@@ -41,9 +39,9 @@ wp db query "DELETE FROM wpcrean_posts WHERE post_type = 'revision'"
 wp cache flush
 wp transient delete-all
 
-# TODO: Do a search-replace on the entire database for site URL
-#echo -e "==> Search/replace hostname…"
-#wp search-replace production.site crean.dev
+# Do a search-replace on the entire database for site URL
+echo -e "==> Search/replace hostname…"
+wp search-replace https://cremoznik.si http://crean.dev
 
 echo -e "==> Flushing rewrite rules…"
 wp rewrite flush
